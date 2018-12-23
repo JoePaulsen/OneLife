c91c653459e508ab09cf392ba581eb0083bdb5d1
JoePaulsen <jcpaulsen588@gmail.com>
2018-12-22 21:06:26 -0500

    Change jasons pull to my repo

--- scripts/pullAndBuildTestSystem.sh
+++ scripts/pullAndBuildTestSystem.sh
@@ -7,12 +7,12 @@ fi
 
 if [ ! -e OneLife ]
 then
-	git clone https://github.com/jasonrohrer/OneLife.git
+	git clone https://github.com/JoePaulsen/OneLife.git
 fi
 
 if [ ! -e OneLifeData7 ]
 then
-	git clone https://github.com/jasonrohrer/OneLifeData7.git	
+	git clone https://github.com/JoePaulsen/OneLifeData7.git	
 fi
 
 
